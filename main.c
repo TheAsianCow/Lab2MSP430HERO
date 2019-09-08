ebb7cc33a4b957b9ded34bdd81aaedca782980fb
Jerfy <jeffreyhuangdragon@gmail.com>
2019-09-08 14:04:16 -0400

    levels added

--- main.c
+++ main.c
@@ -13,7 +13,7 @@ void clearBoard();
 
 char gameBoard[8][5][2];
 
-enum State{START, INPUT, DESCEND, DEFAULT};
+enum State{START, INPUT, DESCEND, ADVANCE, DEFAULT};
 
 void main(void){
     WDTCTL = WDTPW | WDTHOLD;    // Stop watchdog timer. Always need to stop this!!
@@ -24,8 +24,9 @@ void main(void){
 
     unsigned char currKey = 0;
     unsigned long int loopCnt = 0, i = 0;
-    unsigned int gameStart = 0, difficulty = 0, a = 0;
+    unsigned int gameStart = 0, difficulty = 0, a = 0, aliens = 0, rows = 1;
     enum State s = DEFAULT;
+    char level[10] = "LEVEL 1";
     //loopCnt counts the number of loops during the game, gameStart 0 to not start count, 1 to start counting during game
     //difficulty is used to calculate how fast the aliens descend
 
@@ -34,6 +35,7 @@ void main(void){
             gameBoard[i][a][0] = ((rand() % 6) + 0x30);
             gameBoard[i][a][1] = '\0';
             if(gameBoard[i][a][0] == '0') gameBoard[i][a][0] = ' ';
+            else aliens++;
         }
     }
 
@@ -43,11 +45,16 @@ void main(void){
         if(gameStart){
           if(currKey>=1&&currKey<=5) s = INPUT;
           if(loopCnt%(1000-20*difficulty)==0) s = DESCEND;
+          if(aliens==0) s = ADVANCE;
         }
         switch(s){
           case START:
             //start game countdown
             //only does something if gameStart !=0
+
+            Graphics_clearDisplay(&g_sContext);
+            Graphics_drawStringCentered(&g_sContext, level, AUTO_STRING_LENGTH, 48, 48, TRANSPARENT_TEXT)
+
             gameStart = 1;
             Graphics_clearDisplay(&g_sContext);
             Graphics_drawStringCentered(&g_sContext, "Starting game in: ", AUTO_STRING_LENGTH, 48, 48, TRANSPARENT_TEXT);
@@ -83,6 +90,7 @@ void main(void){
                for(a = 0; a < 5; a++) {
                  if(currKey == gameBoard[i-1][a][0] && currKey!=0){
                      gameBoard[i-1][a][0] = ' ';
+                     aliens--;
                      drawScreen();
                      i=1;
                      a=4;
@@ -98,9 +106,13 @@ void main(void){
                 }
             }
 
-            for(a = 0; a < 5; a++) {
-                gameBoard[0][a][0] = ((rand() % 6) + 0x30);
-                if(gameBoard[0][a][0] == '0') gameBoard[0][a][0] = ' ';
+            if(rows){
+                for(a = 0; a < 5; a++) {
+                    gameBoard[0][a][0] = ((rand() % 6) + 0x30);
+                    if(gameBoard[0][a][0] == '0') gameBoard[0][a][0] = ' ';
+                    else aliens++;
+                }
+                rows--;
             }
             //checks to see if the next descent ends the game
             for(a = 0; a < 5; a++){
@@ -129,6 +141,29 @@ void main(void){
             }
 
             break;
+
+          case ADVANCE:
+              difficulty++;
+              level[6] = difficulty+49;
+              Graphics_drawStringCentered(&g_sContext, level, AUTO_STRING_LENGTH, 48, 48, TRANSPARENT_TEXT);
+              i = 100000;
+              while(i)i--;
+
+              for(i = 0; i < 1; i++){
+                  for(a = 0; a < 5; a++) {
+                      gameBoard[i][a][0] = ((rand() % 6) + 0x30);
+                      gameBoard[i][a][1] = '\0';
+                      if(gameBoard[i][a][0] == '0') gameBoard[i][a][0] = ' ';
+                      else aliens++;
+                  }
+              }
+
+              drawScreen();
+
+              rows = difficulty*4;
+              s = INPUT;
+              break;
+
           default:
             if(!gameStart){
                 Graphics_drawStringCentered(&g_sContext, "SPACE INVADERS", AUTO_STRING_LENGTH, 48, 48, TRANSPARENT_TEXT);
