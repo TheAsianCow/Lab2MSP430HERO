bb029900bf03d8debe7f0555b0b7aa56d269caab
Jerfy <jeffreyhuangdragon@gmail.com>
2019-09-11 15:17:04 -0400

    added starting message and countdown

--- main.c
+++ main.c
@@ -9,7 +9,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-
+struct note{int pitch, duration;char led;};
+struct note song[28];
 
 void main(void){
     WDTCTL = WDTPW | WDTHOLD;    // Stop watchdog timer. Always need to stop this!!
@@ -17,10 +18,65 @@ void main(void){
     initLeds();
     configDisplay();
     configKeypad();
+    configBtn();
 
-    //note will be stored in a struct, which will have pitch, duration, and LED
-    //song will be stored in an array of structs
-    //length of song will depend on what song we choose/how we compose it
-    //the amount of memory taken up by the song will be # of notes * sizeof(struct note)
+    unsigned char currKey;
+    unsigned int gameStart = 0, countDown = 0, i = 0, j = 0;
 
+    //initialize the song
+
+    while (1){
+        currKey = getKey();
+
+        if (currKey == '*'){
+            gameStart = 1;
+            countDown = 1;
+        };
+
+        if (countDown == 1){
+            for (i = 0; i <= 20000; i++){
+                if (i == 0){
+                    Graphics_clearDisplay(&g_sContext);
+                    Graphics_drawStringCentered(&g_sContext, "3",AUTO_STRING_LENGTH,48, 48,TRANSPARENT_TEXT);
+                    Graphics_flushBuffer(&g_sContext);
+                    setUserLED(1);
+                }
+                if (i == 8000){
+                    Graphics_clearDisplay(&g_sContext);
+                    Graphics_drawStringCentered(&g_sContext, "2",AUTO_STRING_LENGTH,48, 48,TRANSPARENT_TEXT);
+                    Graphics_flushBuffer(&g_sContext);
+                    setUserLED(2);
+                }
+                if (i == 16000){
+                    Graphics_clearDisplay(&g_sContext);
+                    Graphics_drawStringCentered(&g_sContext, "1",AUTO_STRING_LENGTH,48, 48,TRANSPARENT_TEXT);
+                    Graphics_flushBuffer(&g_sContext);
+                    setUserLED(1);
+                }
+                if (i == 20000){
+                    Graphics_clearDisplay(&g_sContext);
+                    Graphics_drawStringCentered(&g_sContext, "START",AUTO_STRING_LENGTH,48, 48,TRANSPARENT_TEXT);
+                    Graphics_flushBuffer(&g_sContext);
+                    setUserLED(3);
+                    j = 16000;
+                    while(j)j--;
+                    setUserLED(0);
+                }
+            }
+
+            countDown = 0;
+            Graphics_clearDisplay(&g_sContext);
+            Graphics_drawStringCentered(&g_sContext, "Playing game", AUTO_STRING_LENGTH, 48, 48, TRANSPARENT_TEXT);
+            Graphics_flushBuffer(&g_sContext);
+
+        }
+        else if(gameStart){
+
+        }
+        else{
+            Graphics_drawStringCentered(&g_sContext, "MSP430 HERO", AUTO_STRING_LENGTH, 48, 48, TRANSPARENT_TEXT);
+            Graphics_flushBuffer(&g_sContext);
+        }
+
+    }
 }
